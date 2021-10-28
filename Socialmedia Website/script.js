b7195f25a218ea7fa9c71801321513f4727d95b7
AMAN <kumawatbaman@gmail.com>
2021-10-28 08:50:25 +0530

    Adding Socialmedia Website

---
+++ Socialmedia Website/script.js
@@ -0,0 +1,29 @@
+var settingsmenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn");
+function settingsMenuToggle(){
+   settingsmenu.classList.toggle("settings-menu-height");
+}
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme") == "light"){
+        localStorage.setItem("theme", "dark");
+    }
+    else{
+        localStorage.setItem("theme", "light");
+    }
+
+}
+
+if(localStorage.getItem("theme") == "light"){
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme") == "dark"){
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+else{
+    localStorage.setItem("theme", "light");
+}
