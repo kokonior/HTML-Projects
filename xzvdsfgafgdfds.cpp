9e5fd9142f631c6fcf0a8b73514bf8d4a9a08f9f
Manish102003 <115014067+Manish102003@users.noreply.github.com>
2022-10-10 21:17:48 +0530

    Create xzvdsfgafgdfds.cpp

---
+++ xzvdsfgafgdfds.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float n1, n2, n3;
+
+    cout << "Enter three numbers: ";
+    cin >> n1 >> n2 >> n3;
+
+    if (n1 >= n2) {
+        if (n1 >= n3)
+            cout << "Largest number: " << n1;
+        else
+            cout << "Largest number: " << n3;
+    }
+    else {
+        if (n2 >= n3)
+            cout << "Largest number: " << n2;
+        else
+            cout << "Largest number: " << n3;
+    }
+
+    return 0;
+}
