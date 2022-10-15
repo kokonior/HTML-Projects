a463fb6c582125c0885da76f0749c9cbd1d22d0b
Mehul Kumar <45559139+WonderSTK@users.noreply.github.com>
2022-10-15 12:05:30 +0530

    Login And Registration page added

---
+++ Sliding Login Form/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('#goRight').on('click', function(){
+    $('#slideBox').animate({
+      'marginLeft' : '0'
+    });
+    $('.topLayer').animate({
+      'marginLeft' : '100%'
+    });
+  });
+  $('#goLeft').on('click', function(){
+    $('#slideBox').animate({
+      'marginLeft' : '50%'
+    });
+    $('.topLayer').animate({
+      'marginLeft': '0'
+    });
+  });
+});
