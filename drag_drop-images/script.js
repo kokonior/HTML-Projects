a3c864f4afe60153fa59730bbb9ec94271bd0ca9
Amit Barman <amitbarrman15fifteen@gmail.com>
2022-10-16 03:45:34 +0530

    update everything

--- drag_drop-images/script.js
+++ drag_drop-images/script.js
@@ -1,11 +1,13 @@
 // required elements
 
-const container=document.querySelector("body");
+const container=document.querySelector(".container");
 const dragtext=container.querySelector("h1");
 const btn=container.querySelector("button");
 const ip=container.querySelector("input");
 
-button.onclick=()=>{ip.click();}
+btn.onclick=()=>{
+    ip.click();
+}
 let file;
 
 //call activate function
@@ -15,17 +17,29 @@ function activate(){
     showFile();
 }
 
+
+container.addEventListener("dragover",(event)=>{
+    event.preventDefault();
+    dragtext.textContent = "Release to upload"
+});
+
+container.addEventListener("drop",(event)=>{
+    event.preventDefault();
+    file = event.dataTransfer.files[0];
+    showFile();
+});
+
 //call showFile function
 
 function showFile(){
     let fileT=file.type;
-    let valid_ext=["images/jpeg","images/jpg","images/png","application/pdf"];
+    let valid_ext=["image/jpeg","image/jpg","image/png","application/pdf"];
 
     if(valid_ext.includes(fileT)){
         let fileR= new FileReader();
         fileR.onload =()=>{
             let fileURL = fileR.result;
-            let imgTag = `<img src="$(fileURL)" alt="">`;
+            let imgTag = `<div id="img-box"><img src="${fileURL}" alt=""></div>`;
             container.innerHTML = imgTag;
         }
         fileR.readAsDataURL(file);
