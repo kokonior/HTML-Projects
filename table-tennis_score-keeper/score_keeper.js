5ac17fe4a2097d41a694768f1096842e1e2eef23
Sanyam <sanyammrd56@gmail.com>
2022-10-16 21:36:17 +0530

    Added ping-pong score keeper

---
+++ table-tennis_score-keeper/score_keeper.js
@@ -0,0 +1,56 @@
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
+const resetButton = document.querySelector('#reset')
+const winningScoreSelect = document.querySelector('#playTo')
+
+let winningScore = 3;
+let isGameOver = false;
+
+function updateScores(player, opponent) {
+    if (!isGameOver) {
+        player.score++;
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('has-text-success')
+            opponent.display.classList.add('has-text-danger')
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+        player.display.textContent = player.score;
+    }
+}
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2)
+})
+
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1)
+})
+
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value)
+    reset()
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameOver = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger')
+        p.button.disabled = false;
+    }
+}
