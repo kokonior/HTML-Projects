59b4cc1f780e578da90e96084e1d8dc6d30e9604
Juli2211 <116270110+Juli2211@users.noreply.github.com>
2022-10-21 11:13:47 +0530

    Hacktoberfest 2022

---
+++ Loop 3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace  std;
+int main ()
+{ int n,i,fact=1;
+ cout<<"Enter a number:";
+ cin>>n;
+for
+(int i = 1; i <= n;++i )
+
+{ fact=fact*i;}
+
+  cout<<"factorial value="<<fact<<endl;
+  return 0;
+}
+
