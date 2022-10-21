26cd147dae7b9dd5401d2249628aecda04280707
ajaypradeep <ajaypradeep1712@gmail.com>
2022-10-21 15:42:45 +0530

    added admin dashboard snippet/template

---
+++ Admin_Dashboard-template/assets/js/index.js
@@ -0,0 +1,115 @@
+const primaryColor = '#4834d4'
+const warningColor = '#f0932b'
+const successColor = '#6ab04c'
+const dangerColor = '#eb4d4b'
+
+const themeCookieName = 'theme'
+const themeDark = 'dark'
+const themeLight = 'light'
+
+const body = document.getElementsByTagName('body')[0]
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date()
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
+  var expires = "expires="+d.toUTCString()
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/"
+}
+
+function getCookie(cname) {
+  var name = cname + "="
+  var ca = document.cookie.split(';')
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1)
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+  return ""
+}
+
+loadTheme()
+
+function loadTheme() {
+	var theme = getCookie(themeCookieName)
+	body.classList.add(theme === "" ? themeLight : theme)
+}
+
+function switchTheme() {
+	if (body.classList.contains(themeLight)) {
+		body.classList.remove(themeLight)
+		body.classList.add(themeDark)
+		setCookie(themeCookieName, themeDark)
+	} else {
+		body.classList.remove(themeDark)
+		body.classList.add(themeLight)
+		setCookie(themeCookieName, themeLight)
+	}
+}
+
+function collapseSidebar() {
+	body.classList.toggle('sidebar-expand')
+}
+
+window.onclick = function(event) {
+	openCloseDropdown(event)
+}
+
+function closeAllDropdown() {
+	var dropdowns = document.getElementsByClassName('dropdown-expand')
+	for (var i = 0; i < dropdowns.length; i++) {
+		dropdowns[i].classList.remove('dropdown-expand')
+	}
+}
+
+function openCloseDropdown(event) {
+	if (!event.target.matches('.dropdown-toggle')) {
+		// 
+		// Close dropdown when click out of dropdown menu
+		// 
+		closeAllDropdown()
+	} else {
+		var toggle = event.target.dataset.toggle
+		var content = document.getElementById(toggle)
+		if (content.classList.contains('dropdown-expand')) {
+			closeAllDropdown()
+		} else {
+			closeAllDropdown()
+			content.classList.add('dropdown-expand')
+		}
+	}
+}
+
+var ctx = document.getElementById('myChart')
+ctx.height = 500
+ctx.width = 500
+var data = {
+	labels: ['January', 'February', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+	datasets: [{
+		fill: false,
+		label: 'Completed',
+		borderColor: successColor,
+		data: [120, 115, 130, 100, 123, 88, 99, 66, 120, 52, 59],
+		borderWidth: 2,
+		lineTension: 0,
+	}, {
+		fill: false,
+		label: 'Issues',
+		borderColor: dangerColor,
+		data: [66, 44, 12, 48, 99, 56, 78, 23, 100, 22, 47],
+		borderWidth: 2,
+		lineTension: 0,
+	}]
+}
+
+var lineChart = new Chart(ctx, {
+	type: 'line',
+	data: data,
+	options: {
+		maintainAspectRatio: false,
+		bezierCurve: false,
+	}
+})
---
+++ Admin_Dashboard-template/readme.md
@@ -0,0 +1,23 @@
+# ADMIN DASHBOARD
+
+*This snippet is build with HTML, CSSS, JAVASCRIPT.*  
+_Basic admin landing dashboard_
+
+
+## PREVIEW
+
+  
+ ![Screenshot (1010)](https://github.com/ajaygitt/Website-Template/blob/ajay/admin-dashboard/Admin%20Dashboard/preview/lightmode.png?raw=true)
+ 
+   
+ ![Screenshot (1010)](https://github.com/ajaygitt/Website-Template/blob/ajay/admin-dashboard/Admin%20Dashboard/preview/darkmode.png?raw=true)
+
+
+
+
+## FEATURES
+
+* Dark Mode
+* Light Mode
+* Responsive
+* Font-awesome icons
